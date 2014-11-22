d1ab29afb6860aa7308a5d0f6bee2ff2df98b37e
Kirik snegovick Konstantin <snegovick@gmail.com>
2014-11-22 22:09:30 +0300

    * Fixed touch control direction

--- game_logic.js
+++ game_logic.js
@@ -321,11 +321,11 @@ GameLogic.prototype = {
     console.log(event);
     self.player.set_touch_shoot(self.player);
     if (self.player.x - event.touches[0].pageX > self.player.x_step) {
-      self.left = false;
-      self.right = true;
-    } else if (self.player.x - event.touches[0].pageX < -self.player.x_step) {
       self.left = true;
       self.right = false;
+    } else if (self.player.x - event.touches[0].pageX < -self.player.x_step) {
+      self.left = false;
+      self.right = true;
     }
   },
 
@@ -341,11 +341,11 @@ GameLogic.prototype = {
     console.log("move");
     console.log(event);
     if (self.player.x - event.touches[0].pageX > self.player.x_step) {
-      self.left = false;
-      self.right = true;
-    } else if (self.player.x - event.touches[0].pageX < -self.player.x_step) {
       self.left = true;
       self.right = false;
+    } else if (self.player.x - event.touches[0].pageX < -self.player.x_step) {
+      self.left = false;
+      self.right = true;
     }    
   },
 
